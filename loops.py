4d706813fd94335e56ebb55c4709d5538b7b9a8b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-24 04:30:14 -0700

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,91 @@
+# Loops : itrreation
+
+# a = [12, 2, 45, 10, 100]
+
+# for i in a:
+#     print(i)
+#     print("ooooooooooooo")
+
+# range(start, end, increment)
+# start = 0
+# end = n - 1
+# increment = 1
+
+# range(1, 4, 2)
+
+# for j in range(1, 7, 2):
+#     print(j)
+
+
+# zx = "quitweiu"
+
+# for i in zx:
+#     print(i)
+#     if i == 't':
+#         break
+
+
+# a = "moris"
+
+# print(f"the name is : {a}")
+
+# print("abc", a, "ajvsjha")
+
+
+# 9 x 1 = 9
+# 9 x 2 = 18
+# ..
+# .
+# .
+# ..
+# 9 x 10 = 90
+
+
+# num= 10
+# for i in range(1,11):
+    # print("9 x",i,"=",9*i)
+    # print(f"9 x {i} = {9*i}")
+    
+    
+# break continue pass
+
+# for i in range(12):
+#     if i == 7:
+#         # break
+#         # continue
+#         pass
+#     print(i)
+
+
+# for i in range(30):
+#     if i % 2 != 0:
+#         # print(i)
+#         continue
+#     print(i)
+
+lt = [23, 10, 30, 2, 200, 40, 90, 2000, 10000]
+
+tol = 0
+st = ""
+
+for i in lt:
+    tol = tol + i  #23, 33
+    
+    st = st + str(str(i) + " + ")
+    
+print(st, "=", tol)
+
+
+# a = "12"
+
+# b = "10"
+
+# c = a + b
+
+# print(c)
+
+
+
+
+
+
