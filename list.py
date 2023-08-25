ef04ee77bfd97a3b9c0a47c0a824bab8fe284d16
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-25 03:57:59 -0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,73 @@
+# List : 
+# Tuple
+# Set 
+# Dictioney
+
+# List : list is a collection of multipal data
+# []
+
+# list : ordedred, allow duplicasy, mutable (changes)
+
+# lst = [12, "hlo", 12.9, 100, 89, 100, 12]
+
+# print(lst)
+
+# print(type(lst))
+
+# print(lst[4])
+
+# slicing
+
+# lst = [12, "hlo", 12.9, 100, 89, 100, 12]
+
+# :
+
+# print(lst[1:4])
+
+# print(lst[3:7])
+
+
+# lst = [12, "hlo", 12.9, 100, 89, 100, 12]
+
+# adding data
+
+# 1) append()
+# 2) insert()
+
+# lst.append(2000)
+
+# lst.insert(1, 2000)
+
+# print(lst)
+
+
+
+
+# lst = [12, "hlo", 12.9, 100, 89, 100, 12]
+
+# remove data
+# 1) pop()
+# 2) remove()
+
+# lst.pop()
+
+# lst.remove(100)
+
+# print(lst)
+
+
+lst = [12, "hlo", 12.9, 100, 89, 100, 12]
+
+for i in lst:
+    print(i)
+
+# lst.clear()
+
+print(lst)
+
+
+
+
+
+
+
