a35b268726b3d0e5bcafff3f3d1321cf0d557bcf
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-28 04:09:37 -0700

    Create tuple-set.py

---
+++ tuple-set.py
@@ -0,0 +1,80 @@
+# List : list is a collection of multipal data
+# []
+
+# list : ordedred, allow duplicasy, mutable (changes)
+
+
+
+
+
+# Tuple : collection of multipla data....
+# ()
+# tuple :  ordered, allow duplicasy, imutable
+
+# tpl = (12, 100, "hlo", 120.8, 100, 100)
+# print(tpl)
+# print(tpl, type(tpl))
+
+# print(tpl[3])
+
+# tpl.insert(1, 1200)
+
+# print(tpl)
+
+
+
+# # type casting  
+
+# a = 10
+# print(a, type(a))
+
+# tpl = (12, 100, "hlo", 120.8, 100, 100)
+# print(tpl)
+# print(type(tpl))
+
+# lst = list(tpl)
+# print(lst)
+# print(type(lst))
+
+# lst.append(1200)
+
+# print(lst)
+
+# tl = tuple(lst)
+
+# print(tl)
+# print(type(tl))
+
+
+
+
+
+
+
+
+# Set : its also a collcetion of multipal data
+# {}
+
+# set : unordered, do not allow duplicasty, mutable
+
+
+st = {12, 100, "hlo", "ok", 100, "hlo", 90, 50, 12}
+
+# print(st)
+# print(type(st))
+
+# adding new data
+
+# st.add(1200)
+
+st.pop()
+
+st.remove(12)
+
+print(st)
+
+
+
+
+
+
