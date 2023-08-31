6d375091626391e0219eae025a429b7112ecb3da
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-31 04:29:12 -0700

    Update start-functions.py

--- start-functions.py
+++ start-functions.py
@@ -52,5 +52,82 @@
 
 
 
+# 2 x 1 = 2
+# 2 x 2 = 4
+# .
+# .
+# .
+# .
+# .
+
+# table(6)
+
+# 2 x 1 = 2
+# a = 4
+# for i in range(1, 11):
+#     print(a, "x", i, "=", a*i)
+    
+# def mytabale(a):
+#     for i in range(1, 11):
+#         print(a, "x", i, "=", a*i)
+    
+# mytabale(2)
+
+
+# b = 4
+# a = 1
+
+# while a < 11:
+#     print(b, "x", a, "=", b*a)
+#     a = a + 1
+
+
+
+# def showmy(b):
+#     a = 1
+#     while a < 11:
+#         print(b, "x", a, "=", b*a)
+#         a = a + 1
+    
+# showmy(8)    
+
+
+
+
+
+
+# define a function
+# call a funciton
+# arguments
+# perameters
+
+# default perameters
+# def info(name = "hlo", age = 10):
+#     print("user name is : ", name)
+#     print("user age is : ", age)
+
+# info("kriss", 30)
+
+# info("moris", 100)
+
+
+
+
+
+# def info(name, age):
+#     print("user name is : ", name)
+#     print("user age is : ", age)
+
+# info(age = 30, name = "moris")
+
+# *args : aribtrary arguments
+# **kwargs : keyword arbitrary arguments
+
+
+
+
+
+
+
 
 
