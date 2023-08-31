d4c73ec9d1fd30b3ef5e2ea8e551873d29c253cf
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-31 03:50:00 -0700

    Create start-functions.py

---
+++ start-functions.py
@@ -0,0 +1,56 @@
+# Functions : 
+# 1) Built in functions
+# print()
+# len()
+# input()
+# list()
+
+# 2) User define function
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+# a = 30
+# b = 100
+# c = a + b
+# print(c)
+
+
+# code optimization  
+
+# decleation of a function
+
+# def xyz():
+#     print("i am a function")
+    
+
+# # call a function
+
+# xyz()
+
+
+# xyz()
+
+    
+# perameters
+# arguments
+
+# perameters
+# def sumthis(x, b, r):
+#     # print(a, b)
+#     c = x + b + r
+#     print(c)
+
+# # arguments
+# # sumthis(100, 200)  
+
+
+# sumthis(2, 7, 10)
+
+
+
+
+
